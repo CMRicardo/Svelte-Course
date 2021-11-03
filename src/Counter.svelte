ae6e6ccf2eda9a91c920fdf3819127b412dae2b7
CMRicardo <ricardocorralas84@gmail.com>
2021-11-03 16:52:39 -0600

    Made counter component

---
+++ src/Counter.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let initialCounter = 0
+	let counter = initialCounter
+
+	const handleClick = () => {
+		counter += 1
+	}
+</script>
+
+<main>
+	<h1>Times {counter}!!!</h1>
+	
+	<button on:click={handleClick}>Increment</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: crimson;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
